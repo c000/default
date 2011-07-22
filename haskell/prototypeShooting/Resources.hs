9cc9c19f0776dbc2397c8a9eb8b5968b90590a32
Sat Prime <sat@archlinux.localdomain>
2011-07-23 00:44:36 +0900

    PrototypeShooting: BackGround

--- haskell/prototypeShooting/Resources.hs
+++ haskell/prototypeShooting/Resources.hs
@@ -16,6 +16,23 @@ data Images = Images
   }
 
 loadImages = do
-  p <- load "PrototypeShootingContent/Images/player.png"
+  bg  <- load "PrototypeShootingContent/Images/background.png"
+  e   <- load "PrototypeShootingContent/Images/enemy.png"
+  lbr <- load "PrototypeShootingContent/Images/LifeBarRed.png"
+  p   <- load "PrototypeShootingContent/Images/player.png"
+  wb  <- load "PrototypeShootingContent/Images/whiteBoard.png"
+  b   <- load "PrototypeShootingContent/Images/bullet.png"
+  lb  <- load "PrototypeShootingContent/Images/LifeBar.png"
+  pb  <- load "PrototypeShootingContent/Images/playerBullet.png"
+  se  <- load "PrototypeShootingContent/Images/strongEnemy.png"
   return $ Images
-    { player = p }
+    { backGround = bg
+    , enemy = e
+    , lifeBarRed = lbr
+    , player = p
+    , whiteBoard = wb
+    , bullet = b
+    , lifeBar = lb
+    , playerBullet = pb
+    , strongEnemy = se
+    }
--- haskell/prototypeShooting/main.hs
+++ haskell/prototypeShooting/main.hs
@@ -1,5 +1,7 @@
 import Data.Set
 import Data.Maybe (catMaybes)
+import System.IO (hFlush, stdout)
+
 import Graphics.UI.SDL as SDL
 
 import qualified Resources as R
@@ -9,13 +11,14 @@ data GameConfig = GameConfig
   , windowHeight :: Int
   }
 
-defaultConfig = GameConfig 600 400
+defaultConfig = GameConfig 400 600
 
 data Game = GameStruct
   { gameWindow :: Surface
   , keyState :: Set SDLKey
   , player :: (Int, Int)
   , images :: R.Images
+  , frames :: Int
   }
 
 initGame (GameConfig w h) = do
@@ -27,6 +30,7 @@ initGame (GameConfig w h) = do
     , keyState = empty
     , player = (0, 0)
     , images = img
+    , frames = 0
     }
 
 quitGame (GameStruct {gameWindow = w}) = do
@@ -56,9 +60,11 @@ stepGame g0@(GameStruct {keyState = keys}) = f g0
     keyList = toList keys
     f :: Game -> Game
     f = foldr1 (.) $
-        id :
+        defaultStep :
         ( catMaybes
-        . (Prelude.map keyToAction)) keyList
+        . (Prelude.map keyToAction)
+        ) keyList
+    defaultStep g@(GameStruct {frames = f}) = g {frames = f+1}
 
 keyToAction :: SDLKey -> Maybe (Game -> Game)
 keyToAction key = case key of
@@ -75,12 +81,22 @@ keyToAction key = case key of
 render
   g0@(GameStruct { gameWindow = w
                  , player = (px, py)
-                 , images = R.Images { R.player = p
+                 , images = R.Images { R.backGround = bg
+                                     , R.player = p
                                      }
+                 , frames = f
                  })
-  = do True <- blitSurface p Nothing w (Just $ Rect px py 0 0)
+  = do bgRendering
+       True <- blitSurface p Nothing w (Just $ Rect px py 0 0)
        SDL.flip w
        return g0
+  where
+    bgRendering = do
+      let positionY = 2 * f `mod` 300
+      True <- blitSurface bg Nothing w (Just $ Rect 0 (positionY-300) 0 0)
+      True <- blitSurface bg Nothing w (Just $ Rect 0  positionY      0 0)
+      True <- blitSurface bg Nothing w (Just $ Rect 0 (positionY+300) 0 0)
+      return ()
 
 main = do
   game <- initGame defaultConfig
